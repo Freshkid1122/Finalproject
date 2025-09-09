08a530e5ddfbc620f30b91377f70942ae29f487b
Fresh_kid <samueladekunle407@gmail.com>
2025-09-09 16:50:07 +0100

    error fix

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -9,46 +9,60 @@ const Sidebar = () => {
          <nav className="sidebar p-2 border-end sidebar-content">
                 <ul className="nav flex-column mt-2">
                   <li className="nav-item">
+                    <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white active" aria-current="page">
                 <i className="bi bi-house me-2"></i>
                 Dashboard
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-list-ul me-2"></i>
                 New Orders
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-plus-circle me-2"></i>
                 Add Products
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-graph-up me-2"></i>
                 Sales
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-people me-2"></i>
                 Customers
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-gear me-2"></i>
                 Settings
               </a>
+              </Link>
             </li>
             <li>
+              <Link to="/resturant/dashboard">
               <a href="#" className="nav-link text-white">
                 <i className="bi bi-bell me-2"></i>
                 Notification
               </a>
+              </Link>
             </li>
                 </ul>
 
