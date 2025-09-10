3babd091ad328aaccf4c8e09fe2419a0e565e261
Fresh_kid <samueladekunle407@gmail.com>
2025-09-10 12:45:37 +0100

    error fix

--- client/components/Last.jsx
+++ client/components/Last.jsx
@@ -10,7 +10,7 @@ const Last = () => (
     bottom: 0,
     width: '100%',
   }}>
-    <p>© {new Date().getFullYear()} Your Project Name. All rights reserved.</p>
+    <p>© {new Date().getFullYear()} Adekunle Samuel Kayode . All rights reserved.</p>
   </footer>
 );
 
